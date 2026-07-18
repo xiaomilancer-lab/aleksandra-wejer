f413da727ab9397d3b5da134fadcf3b50826c1fb
Szymon Wejer <sz.wejer@met-lak.local>
2026-07-18 22:29:20 +0200

    Dodano ręczny Google verification

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="kLEduvFtYgx4dlLGe1VlMiF9U7yNxqWnhg-cQy92ZHg"
+      />
+    </>
+  );
+}
